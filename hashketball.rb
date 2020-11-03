db3131d1ca0ed5effdbcfbf3cbeae131edc0475d
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-03 09:24:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,3 +128,18 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player_name)
+  game_hash.each do |location, teams|
+    player_info = []
+    teams[:players].each do |player|
+      player_info << player
+      binding.pry
+  end
+  #iterate through players
+  # lookup values
+  # add values together
+end
+end
+
+
+  # binding.pry
