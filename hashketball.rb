ff8bfd5cd5af6eecc87fe6c327f34f2d5943c9ac
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-11-03 10:04:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,6 +171,7 @@ def player_numbers(team_name)
     if(teams[:team_name] == team_name)
       new_array = []
       teams[:players].each do |player|
+        new_array << player[:number]
         binding.pry
       end
     end
